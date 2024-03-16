ba3c80d9e26a64887c186f838d7c48ad799dde96
Vlados <89387622+nebel310@users.noreply.github.com>
2024-03-16 12:30:18 +0300

    Update passwords.py

--- passwords.py
+++ passwords.py
@@ -32,3 +32,49 @@ with open('students_password.csv', 'w', newline='', encoding='utf8') as file:
     w = csv.DictWriter(file, fieldnames=['id', 'Name', 'titleProject_id', 'class', 'score', 'login', 'password'])
     w.writeheader()
     w.writerows(students_with_password)
+#-----------------------------------------------------
+
+import csv
+import string
+import random
+
+def initial(s: string):
+    """
+    Создает инициалы на основе имени.
+
+    Args:
+        s (string): Имя студента.
+
+    Returns:
+        string: Инициалы, сформированные на основе первой буквы имени, фамилии и отчества.
+    """
+    print(s)
+    names = s.split()
+    print(names)
+    return f'{names[0]}_{names[1][0]}{names[2][0]}'
+
+def create_password():
+    """
+    Генерирует случайный пароль.
+
+    Returns:
+        string: Сгенерированный случайный пароль.
+    """
+    characters = string.ascii_letters + string.digits
+    print(characters)
+    password = ''.join(random.choice(characters) for _ in range(8))
+    return password
+
+students_with_password = []
+
+with open('students.csv', encoding='utf-8') as file:
+    reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
+    for row in reader:
+        row['login'] = initial(row['Name'])
+        row['password'] = create_password()
+        students_with_password.append(row)
+
+with open('students_password.csv', 'w', newline='', encoding='utf8') as file:
+    w = csv.DictWriter(file, fieldnames=['id', 'Name', 'titleProject_id', 'class', 'score', 'login', 'password'])
+    w.writeheader()
+    w.writerows(students_with_password)
