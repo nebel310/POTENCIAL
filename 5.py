efa11349d514a6fbc5aab9a57d5085e5008c616f
Vlados <89387622+nebel310@users.noreply.github.com>
2024-03-16 12:15:16 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -16,14 +16,14 @@ def generate_hash(s: str):
 
 
 students_with_hash = []
-with open('D:/My Projects/programming/hueta/students.csv', encoding='utf-8') as file:
+with open('students.csv', encoding='utf-8') as file:
     reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
     for row in reader:
         row['id'] = generate_hash(row['Name'])
         students_with_hash.append(row)
 
 
-with open('D:/My Projects/programming/hueta/students_new.csv', 'w', newline='', encoding='utf-8') as file:
+with open('students_with_hash.csv', 'w', newline='', encoding='utf-8') as file:
     w = csv.DictWriter(file, fieldnames=['id', 'Name', 'titleProject_id', 'class', 'score', 'login', 'password'])
     w.writeheader()
     w.writerows(students_with_hash)
