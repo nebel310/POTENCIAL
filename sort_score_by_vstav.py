d18d8e22159a43157674b9e1057eacb05107864f
Vlados <89387622+nebel310@users.noreply.github.com>
2024-03-16 12:33:49 +0300

    Update sort_score_by_vstav.py

--- sort_score_by_vstav.py
+++ sort_score_by_vstav.py
@@ -24,3 +24,34 @@ for el in reader:
         count += 1
     if count == 4:
         break
+#-----------------------------------------------------------
+
+import csv
+
+
+with open('students.csv', encoding='utf8') as file:
+    reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
+    # Сортируем список студентов по оценкам в порядке возрастания
+    for i in range(1, len(reader)):
+        for j in range(i, 0, -1):
+            if float(reader[j]['score'] if reader[j]['score'] != 'None' else 0) < float(reader[j-1]['score'] if reader[j-1]['score'] !='None' else 0):
+                reader[j], reader[j-1] = reader[j-1], reader[j]
+            else:
+                break
+    # Разворачиваем список, чтобы оценки шли в порядке убывания
+    reader = reader[::-1]
+
+print(reader)
+
+print('10 класс')
+count = 1
+
+# Выводим информацию о студентах из 10 класса, начиная с самого лучшего результата
+for el in reader:
+    if '10' in el['class']:
+        surname, name, patronumic = el['Name'].split()
+        print(f'{count} место: {name[0]}. {surname}')
+        count += 1
+    if count == 4:
+        break
+
